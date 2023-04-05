f484e48041086ea41068660c0b5106faaf59c455
Jiaming Zeng <zjiaming@eecs583a.eecs.umich.edu>
2023-04-04 22:27:37 -0400

    Added all

---
+++ build/mypass/CMakeFiles/LLVMHW2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLVMHW2.dir/DSWP_0.cpp.o"
+  "CMakeFiles/LLVMHW2.dir/DSWP_0.cpp.o.d"
+  "LLVMHW2.pdb"
+  "LLVMHW2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLVMHW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
